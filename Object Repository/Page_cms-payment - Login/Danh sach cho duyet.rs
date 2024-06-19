51cbe4a65a090adf8178c53d751b1866b7f9c214
hongnt97 <hongnt@9pay.vn>
2024-06-19 17:56:37 +0700

    refund 123

---
+++ Object Repository/Page_cms-payment - Login/Danh sach cho duyet.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Danh sach cho duyet</name>
+   <tag></tag>
+   <elementGuidId>2db8aef5-1a6e-42e3-99d1-0260c57afab9</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@id=&quot;side-menu&quot;]/li[4]/ul/li[2]/a</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/Page_cms-payment - Login/yeu cau hoan tien.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>yeu cau hoan tien</name>
+   <tag></tag>
+   <elementGuidId>ec52e86f-c26d-403f-a3be-a84ea38be7d4</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>/html/body/div[2]/nav/div/ul/li[4]/a</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
